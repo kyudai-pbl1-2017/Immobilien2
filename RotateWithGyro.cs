e533d945d80a272963a60f4cc4fda38499508955
shimura <shimura@f.ait.kyushu-u.ac.jp>
2017-12-04 14:40:59 +0900

    add new file

---
+++ RotateWithGyro.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWithGyro : MonoBehaviour {
+
+    void Start ()
+    {
+        Input.gyro.enabled = true;
+    }
+
+    void Update ()
+    {
+        Quaternion gattitude = Input.gyro.attitude;
+        gattitude.x *= -1;
+        gattitude.y *= -1;
+        transform.localRotation =
+            Quaternion.Euler(90, 0, 0) * gattitude;
+    }
+}
